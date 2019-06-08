d0bc062e73a00dc2ad512c7b1bde2b864b5f8392
Abouu <mabouu21@gmail.com>
2019-06-08 16:52:16 +0200

    Replace $db for $dbase for minimum length 3

--- model/backend/MailManager.php
+++ model/backend/MailManager.php
@@ -6,16 +6,16 @@ class MailManager extends Manager
 {
 	public function getMails()
 	{
-		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, name, email, subject, content, date_format(date_receipt, \'%d/%m/%Y à %Hh%imin%ss\') AS date_recu, status FROM Email ORDER BY id DESC');
+		$dbase = $this->dbConnect();
+		$req = $dbase->query('SELECT id, name, email, subject, content, date_format(date_receipt, \'%d/%m/%Y à %Hh%imin%ss\') AS date_recu, status FROM Email ORDER BY id DESC');
 
 		return $req;
 	}
 
 	public function receipt($name, $email, $subject, $message)
 	{
-		$db = $this->dbConnect();
-		$req = $db->prepare('INSERT INTO Email(name, email, subject, content, date_receipt) VALUES(?, ?, ?, ?, NOW())');
+		$dbase = $this->dbConnect();
+		$req = $dbase->prepare('INSERT INTO Email(name, email, subject, content, date_receipt) VALUES(?, ?, ?, ?, NOW())');
 		$req->execute(array($name, $email, $subject, $message));
 
 		return $req;
--- model/frontend/PostManager.php
+++ model/frontend/PostManager.php
@@ -7,16 +7,16 @@ class PostManager extends Manager
 {
 	public function getPosts()
 	{
-		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE status = \'publié\' ORDER BY id DESC LIMIT 0,6');
+		$dbase = $this->dbConnect();
+		$req = $dbase->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE status = \'publié\' ORDER BY id DESC LIMIT 0,6');
 
 		return $req;
 	}
 
 	public function getPost($postId)
 	{
-		$db = $this->dbConnect();
-		$req = $db->prepare('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE id = ?');
+		$dbase = $this->dbConnect();
+		$req = $dbase->prepare('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE id = ?');
 		$req->execute(array($postId));
 		$post = $req->fetch();
 
@@ -25,16 +25,16 @@ class PostManager extends Manager
 
 	public function getallPosts()
 	{
-		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE status = \'publié\' ORDER BY id DESC');
+		$dbase = $this->dbConnect();
+		$req = $dbase->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts WHERE status = \'publié\' ORDER BY id DESC');
 
 		return $req;
 	}
 
 	public function addPost($alias, $title, $content, $userId)
 	{
-		$db = $this->dbConnect();
-		$req = $db->prepare('INSERT INTO posts(author_id, author, title, content, creation_date, modification_date) VALUES(?, ?, ?, ?, NOW(), NOW())');
+		$dbase = $this->dbConnect();
+		$req = $dbase->prepare('INSERT INTO posts(author_id, author, title, content, creation_date, modification_date) VALUES(?, ?, ?, ?, NOW(), NOW())');
 		$req->execute(array($userId, $alias, $title, $content)); 
 
 		return $req;
@@ -42,25 +42,25 @@ class PostManager extends Manager
 
 	public function postAdmin()
 	{
-		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, author_id, title, date_format(modification_date, \'%d/%m/%Y\') AS date_modification FROM posts ORDER BY id DESC LIMIT 0,6');
+		$dbase = $this->dbConnect();
+		$req = $dbase->query('SELECT id, author_id, title, date_format(modification_date, \'%d/%m/%Y\') AS date_modification FROM posts ORDER BY id DESC LIMIT 0,6');
 
 		return $req;
 	}
 
 	public function getListPost()
 	{
-		$db = $this->dbConnect();
-		$req = $db->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts ORDER BY id DESC');
+		$dbase = $this->dbConnect();
+		$req = $dbase->query('SELECT id, author, title, content, date_format(creation_date, \'%d/%m/%Y\') AS date_publication FROM posts ORDER BY id DESC');
 
 		return $req;
 	}
 
 	public function count($authorId)
 	{
-		$db = $this->dbConnect();
+		$dbase = $this->dbConnect();
 
-		$req = $db->prepare('SELECT COUNT(*) AS nbrepost FROM posts WHERE author_id = ?');
+		$req = $dbase->prepare('SELECT COUNT(*) AS nbrepost FROM posts WHERE author_id = ?');
 		$req->execute(array($authorId));
 		$post = $req->fetch();
 
@@ -69,9 +69,9 @@ class PostManager extends Manager
 
 	public function userPost($userId)
 	{
-		$db = $this->dbConnect();
+		$dbase = $this->dbConnect();
 
-		$req = $db->prepare('SELECT id, author, title, date_format(modification_date, \'%d/%m/%Y à %Hh%i\') AS date_modification FROM posts WHERE author_id = ? AND status = \'publié\' ');
+		$req = $dbase->prepare('SELECT id, author, title, date_format(modification_date, \'%d/%m/%Y à %Hh%i\') AS date_modification FROM posts WHERE author_id = ? AND status = \'publié\' ');
 		$req->execute(array($userId));
 
 		return $req;
@@ -79,9 +79,9 @@ class PostManager extends Manager
 
 	public function postSelect($postId)
 	{
-		$db = $this->dbConnect();
+		$dbase = $this->dbConnect();
 
-		$req = $db->prepare('SELECT id, author_id, author, title, content FROM posts WHERE id = ?');
+		$req = $dbase->prepare('SELECT id, author_id, author, title, content FROM posts WHERE id = ?');
 		$req->execute(array($postId));
 
 		$post = $req->fetch();
@@ -91,9 +91,9 @@ class PostManager extends Manager
 
 	public function upPost($title, $content, $postId)
 	{
-		$db = $this->dbConnect();
+		$dbase = $this->dbConnect();
 
-		$req = $db->prepare('UPDATE posts SET title = ?, content = ?, modification_date = NOW() WHERE id = ?;');
+		$req = $dbase->prepare('UPDATE posts SET title = ?, content = ?, modification_date = NOW() WHERE id = ?;');
 		$req->execute(array($title, $content, $postId));
 
 		return $req;
@@ -101,9 +101,9 @@ class PostManager extends Manager
 
 	public function delete($postId)
 	{
-		$db = $this->dbConnect();
+		$dbase = $this->dbConnect();
 
-		$req = $db->prepare('UPDATE posts SET status = \'suspendu\' WHERE id = ?');
+		$req = $dbase->prepare('UPDATE posts SET status = \'suspendu\' WHERE id = ?');
 		$req->execute(array($postId));
 
 		return $req;
